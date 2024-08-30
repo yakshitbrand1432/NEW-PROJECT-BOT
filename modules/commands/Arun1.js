2c3a7c63bf0f937103b02f7e1a523ab46bb35f87
PiyushA2Ztricks <149769315+PiyushA2Ztricks@users.noreply.github.com>
2024-08-30 13:49:51 +0530

    Update Arun1.js

--- modules/commands/Arun1.js
+++ modules/commands/Arun1.js
@@ -1,9 +1,9 @@
  const fs = require("fs");
 module.exports.config = {
-	name: "Arun",
+	name: "Piyush",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Arun", 
+	credits: "Piyush", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
@@ -14,10 +14,10 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("owner") ||
-     react.includes("Arun") || 
+     react.includes("Piyush") || 
 react.includes("arun")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐏𝐢𝐲𝐮𝐬𝐡 𝐂𝐡𝐚𝐮𝐡𝐚𝐧✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@piyush_chauhan447?si=I4zq1guUJ603tmVp ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋 https://chat.whatsapp.com/FvSKvHB3OQ4ItfouW8c3IY `",
 				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
