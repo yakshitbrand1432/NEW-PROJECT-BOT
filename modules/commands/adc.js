1fd75e2b41120b0d04a11f173ac60c71b019d330
PiyushA2Ztricks <149769315+PiyushA2Ztricks@users.noreply.github.com>
2024-08-30 13:56:14 +0530

    Update adc.js

--- modules/commands/adc.js
+++ modules/commands/adc.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "adc",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "Arun",
+    credits: "Piyush",
     description: "Apply code from buildtooldev and pastebin",
     commandCategory: "Admin",
     usages: "[reply or text]",
