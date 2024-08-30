bccd8de34a16d4d6925b5eada1a37f1386484046
PiyushA2Ztricks <149769315+PiyushA2Ztricks@users.noreply.github.com>
2024-08-30 13:58:34 +0530

    Update admin.js

--- modules/commands/admin.js
+++ modules/commands/admin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100040426712109"];
+            const god = ["100070492726798"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
